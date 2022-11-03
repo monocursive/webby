3c536b8d0dfbccf7ae5ab497b98a4c657195ac8d
Monocursive <cursive@protonmail.ch>
2022-11-03 10:52:31 +0100

    part2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use std::net::SocketAddr;
 
-use axum::{routing::get, Router};
+use axum::{
+    body::Body,
+    extract,
+    extract::Query,
+    response::Html,
+    routing::{get, post, MethodRouter},
+    Json, Router,
+};
+
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/hello", get(greet));
+    let app = Router::new()
+        .merge(greet())
+        .merge(greet_enhanced())
+        .merge(greet_json());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
 
@@ -14,6 +26,46 @@ async fn main() {
         .unwrap();
 }
 
-async fn greet() -> &'static str {
-    "Hello you!"
+fn route(path: &str, method_router: MethodRouter<Body>) -> Router {
+    Router::new().route(path, method_router)
+}
+
+fn greet() -> Router {
+    async fn handler() -> &'static str {
+        "Hello you!"
+    }
+    route("/hello", get(handler))
+}
+
+#[derive(Deserialize)]
+struct GreetParameter {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct CreateGreeting {
+    name: String,
+}
+#[derive(Deserialize, Serialize)]
+struct Greeting {
+    name: String,
+    hello: String,
+}
+
+fn greet_enhanced() -> Router {
+    async fn handler(Query(params): Query<GreetParameter>) -> Html<String> {
+        Html(format!("<h1>Hello {}</h1>", params.name))
+    }
+    route("/greet", get(handler))
+}
+
+fn greet_json() -> Router {
+    async fn handler(extract::Json(payload): extract::Json<CreateGreeting>) -> Json<Greeting> {
+        let greeting = Greeting {
+            name: payload.name,
+            hello: "world".to_string(),
+        };
+        Json(greeting)
+    }
+    route("/greet_json", post(handler))
 }
